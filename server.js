3c8675de2c000e22e295d61c43b323cd67d79f34
modelerzkl <tk817jp@gmail.com>
2025-01-02 21:07:02 +0900

    .

--- server.js
+++ server.js
@@ -13,9 +13,11 @@ app.use(express.static('public'));
 const addressLabels = {
   '0xC9A3Cf506180757AcfCbE8D78B73E5335926e65B': 'Community Treasury',
   '0x82C1889F00EfcDaB3Cde8Ce2DBAAEa57f8Dd6D0B': 'Ecosystem Development',
+  '0x9F2891F29efABd0016a0Aa30F4Dc5C866a3b00dE': 'Ecosystem Development2',
   '0x223e33eBBD7005D5a7C6ef4BAA35eBd74C691D79': 'Team & Advisors',
   '0x262cac775BBe38f161275B5d25bD365B20a2Ed00': 'Early Private Purchasers',
   '0x2123f6d10B580BAf5Eb25a16Bf62F2782cc514C6': 'Liquidity Reserve',
+  '0xC407E348DC24d80201396864c588e0dec42a70FE': 'Liquidity Reserve2',
   '0xf89d7b9c864f589bbF53a82105107622B35EaA40': 'Bybit',
   '0x0D0707963952f2fBA59dD06f2b425ace40b492Fe': 'gate.io'
 };
